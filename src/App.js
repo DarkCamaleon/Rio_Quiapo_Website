ce4510d3a0e6392b3e52e6f7676ae0aadd612def
Alejandro Venegas <alejandro.b.venegas.ch@gmail.com>
2020-10-21 00:10:04 -0300

    alejandro cambios

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App () {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>Cesar estuvo aqui</p>
+        <p> alejandro hizo una segunda modificacion :P</p>
         <a
           className='App-link'
           href='https://reactjs.org'
